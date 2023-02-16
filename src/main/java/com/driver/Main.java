fa93bde65b1fe0a31ddb22d3eb68df1a92b07422
srinivasu jakka <jakkasrinivasu2001@gmail.com>
2023-02-16 22:57:41 +0530

    attempt 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,21 @@
 package com.example;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
 
         // try setting a value to name by directly accessing it using obj
-        obj.name = "John"; // This will give an error as name is a private member variable
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        obj.name = s; // This will give an error as name is a private member variable
 
         // try printing the value of name
         System.out.println(obj.name); // This will also give an error as name is a private member variable
 
         // set the value of name using setter function
-        obj.setName("John");
+        obj.setName(s);
 
         // get the value of name using getter function
         String name = obj.getName();
